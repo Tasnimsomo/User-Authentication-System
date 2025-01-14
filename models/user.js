348ba1aa5aca9d27c3c7993635765b8ee58c5408
Tasnimsomo <tasnasomo@gmail.com>
2025-01-14 22:34:12 +0400

    initial project setup with express and user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     "name": String,
     "email": String,
-    "password": String,
+    "password": {
+        type: String,
+        min: 6,
+        max: 12
+    },
+
     "role": String
 });
 
