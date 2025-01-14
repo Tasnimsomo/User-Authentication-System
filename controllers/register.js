f3e23bb3fb8437bc290eb9d7d322bf247085ecf6
Tasnimsomo <tasnasomo@gmail.com>
2025-01-14 22:48:51 +0400

    add user registration endpoint

--- controllers/register.js
+++ controllers/register.js
@@ -3,29 +3,39 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 exports.register = async (req, res) => {
-    const { firstname, lastName, email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
+    // validate input fields 
     if (!firstName || !lastName || !email || !password) {
         return res.status(400).json({ message: 'Please enter all fields' });
     }
 
     // Check for existing user
-    const user = await User.find(email);
-    if (user) {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
         return res.status(400).json({ message: 'User already exists, please login' });
     }
 
+
+    // hash password before saving
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create new user and save to database 
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+
 
         const newUser = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
+            firstName,
+            lastName,
+            email,
             password: hashedPassword
         });
 
         const user = await newUser.save();
+        res.status(201).json({
+            message: 'User created successfully',
+            user
+        });
     }
     catch (err) {
         res.status(500).json(err);
