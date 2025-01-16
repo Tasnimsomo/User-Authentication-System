5213a95b6d524753ed8fad860c8e35a95c7c0ad5
Tasnimsomo <tasnasomo@gmail.com>
2025-01-16 10:14:32 +0400

    add user login endpoint

--- routes/login_routes.js
+++ routes/login_routes.js
@@ -1,9 +1,11 @@
-const authenticateToken = require('../middleware/authenticateToken');
-
 const router = require('express').Router();
-const { loginController } = require('../controllers/loginController');
+const { login } = require('../controllers/login');
+const authenticateToken = require('../middleware/auth');
 
-router.post('/', loginController);
+
+router.post('/login', login);
 router.get('/profile', authenticateToken, (req, res) => {
     res.json({ message: 'Welcome to the profile page' });
 });
+
+module.exports = router;
