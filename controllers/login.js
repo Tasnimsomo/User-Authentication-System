15cfbf8506ebd644abdab2935d8513d085a18d48
Tasnimsomo <tasnasomo@gmail.com>
2025-01-16 10:12:54 +0400

    add login functionality

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('../models/User');
+const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv').config();
@@ -14,13 +14,13 @@ exports.login = async (req, res) => {
 
         // if user does not exist, return error
         if (!user) {
-            res.status(400).json({ message: "user not found" });
+            return res.status(400).json({ message: "User not found" });
         }
 
         // check if password is valid
-        const isPasswordValid = bcrypt.compare(user.pasword, password);
+        const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
-            res.status(400).json({ message: "invalid password" });
+            return res.status(400).json({ message: "Invalid password" });
         }
 
         // create token
@@ -33,12 +33,14 @@ exports.login = async (req, res) => {
             { expiresIn: "1h" }
         );
 
+        // send response with token
         res.status(200).json({
             message: "Successfully logged in",
             token,
         });
     }
     catch (error) {
+        console.error(error);
         res.status(500).json({ message: "Internal server error" });
     }
-}
+};
