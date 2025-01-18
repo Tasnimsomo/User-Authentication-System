dc4d0a0af024617c7032a7e71cc43f0793a816cb
Tasnimsomo <tasnasomo@gmail.com>
2025-01-18 21:26:34 +0400

    implement rate limiting for login

---
+++ middleware/rateLimiter.js
@@ -0,0 +1,10 @@
+const rateLimit = require('express-rate-limit');
+
+const loginLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000,   // 5 minute window
+    max: 10,                    // 10 attempts
+    message: {
+        status: 'error',
+        message: 'Too many attempts. Try again in 5 minutes'
+    }
+});
