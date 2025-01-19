2092f28deb4260a3fd13a82b9e1bf75de7b62771
Tasnimsomo <tasnasomo@gmail.com>
2025-01-19 21:35:37 +0400

    user login endpoint

--- routes/login_routes.js
+++ routes/login_routes.js
@@ -5,7 +5,7 @@ const rateLimiter = require('../middleware/rateLimiter');
 
 router.post('/login', rateLimiter, login);
 router.get('/profile', authenticateToken, (req, res) => {
-    res.json({ message: 'Welcome `${req.user.email}`' });
+    res.json({ message: `Welcome ${req.user.email}` });
 });
 
 module.exports = router;
