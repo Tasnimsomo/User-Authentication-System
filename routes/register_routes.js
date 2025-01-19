ddab979561d22800dafb4bf0cfeb232c47cdc828
Tasnimsomo <tasnasomo@gmail.com>
2025-01-19 21:36:13 +0400

    user registration endpoint

--- routes/register_routes.js
+++ routes/register_routes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { register } = require('../controllers/register');
 
-
-
 router.post('/register', register);
 
 module.exports = router;
