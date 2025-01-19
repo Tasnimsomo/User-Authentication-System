1b8b9480580ffd176c3220a3a6fc5989b47bf8a2
Tasnimsomo <tasnasomo@gmail.com>
2025-01-19 21:34:27 +0400

    authentication middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,14 +5,14 @@ const authenticateToken = async (req, res, next) => {
         const authHeader = req.headers.authorization;
         const token = authHeader && authHeader.split(' ')[1];
         if (!token) {
-            return res.sendStatus(401).json({ message: "No token provided" });
+            return res.status(401).json({ message: "No token provided" });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
-        return res.sendStatus(403).json({ message: "Invalid token" });
+        return res.status(403).json({ message: "Invalid token" });
     }
 }
 
