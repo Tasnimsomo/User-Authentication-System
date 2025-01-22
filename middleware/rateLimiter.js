693ca6c4e51b038cc52a6fd5be58122d099fa199
Tasnimsomo <tasnasomo@gmail.com>
2025-01-22 10:26:06 +0400

    implement rate limiting for login && git push

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
     windowMs: 5 * 60 * 1000,   // 5 minute window
-    max: 5,                    // 10 attempts
+    max: 5,                    // 5 attempts
     message: {
         status: 'error',
         message: 'Too many attempts. Try again in 5 minutes'
